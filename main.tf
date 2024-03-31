172f5ac062f2e63747f6c8795ecbf7f0ad1ed85b
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-03-31 18:37:29 -0400

    Use versioned k8s objects, use templates for values

--- main.tf
+++ main.tf
@@ -1,50 +1,50 @@
-resource "kubernetes_namespace" "headlamp" {
+locals {
+    configure_ingress = var.host != null
+}
+
+resource "kubernetes_namespace_v1" "headlamp" {
     metadata {
         name = var.namespace
     }
 }
 
-locals {
-    configure_ingress = var.host != null
-}
-
 resource "helm_release" "headlamp" {
-    namespace  = kubernetes_namespace.headlamp.metadata[0].name
+    namespace  = kubernetes_namespace_v1.headlamp.metadata[0].name
     name       = "headlamp"
     repository = "https://headlamp-k8s.github.io/headlamp"
     chart      = "headlamp"
     version    = var.headlamp_version
     values     = [
-        !local.configure_ingress ? "" : <<-EOT1
-        ingress:
-          enabled: true
-          hosts:
-            - host: ${var.host}
-              paths:
-                - path: /
-                  type: Prefix
-        EOT1
-    ,
-        !local.configure_ingress || var.ingress_class == null ? "" : <<-EOT2
-        ingress:
-          annotations:
-            kubernetes.io/ingress.class: ${var.ingress_class}
-        EOT2
-    ,
-        !local.configure_ingress || var.issuer_name == null ? "" : <<-EOT3
-        config:
-          oidc:
-            issuerURL: "${var.oidc_issuer_url}"
-            clientID: "${var.oidc_client_id}"
-            clientSecret: "${var.oidc_client_secret}"
-            scopes: "${var.oidc_scopes}"
-        ingress:
-          annotations:
-            cert-manager.io/${var.issuer_type}: ${var.issuer_name}
-          tls:
-            - secretName: headlamp-tls
-              hosts:
-                - ${var.host}
-        EOT3
+        <<EOT
+%{ if local.configure_ingress ~}
+ingress:
+  enabled: true
+  annotations: {
+%{ if var.issuer_name != null ~}
+    cert-manager.io/${var.issuer_type}: ${var.issuer_name}
+%{ endif ~}
+%{ if var.ingress_class != null ~}
+    kubernetes.io/ingress.class: ${var.ingress_class}
+%{ endif ~}
+  }
+  hosts:
+    - host: ${var.host}
+      paths:
+        - path: /
+          type: Prefix
+%{ if var.issuer_name != null ~}
+  tls:
+    - secretName: headlamp-tls
+      hosts:
+        - ${var.host}
+config:
+  oidc:
+    issuerURL: "${var.oidc_issuer_url}"
+    clientID: "${var.oidc_client_id}"
+    clientSecret: "${var.oidc_client_secret}"
+    scopes: "${var.oidc_scopes}"
+%{ endif ~}
+%{ endif ~}
+EOT
     ]
 }
